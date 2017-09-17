b8fe8bf0fc264e81e766881e7e832b6e93cab64a
João Sardinha <johnsardine@gmail.com>
2017-09-17 21:29:07 +0100

    Init with basic layout and dashboard placeholder

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,52 @@
 <template>
-  <div id="app">
-    <main>
-      <router-view></router-view>
-    </main>
+  <div id="app" class="App">
+    <header class="AppHeader">
+      <div class="AppHeader__brand">Admin Panel</div>
+      <div class="AppHeader__navigation">
+        <ul class="NavBar">
+          <li class="NavBar__item"><a href="#" class="NavBar__cell">Dashboard</a></li>
+          <li class="NavBar__item">
+            <a href="#" class="NavBar__cell">Has children</a>
+            <ul class="NavBar__panel">
+              <li><a href="#">Dashboard</a></li>
+              <li><a href="#">Another one</a></li>
+              <li><a href="#">About</a></li>
+            </ul>
+          </li>
+          <li class="NavBar__item">
+            <a href="#" class="NavBar__cell">Has Mega</a>
+            <div class="NavBar__panel">
+              <p>Anything</p>
+            </div>
+          </li>
+          <li class="NavBar__item"><a href="#" class="NavBar__cell">About</a></li>
+          <li class="NavBar__item"><div class="NavBar__cell App__version">v0.1.0</div></li>
+        </ul>
+      </div>
+    </header>
+    <div class="AppContainer">
+      <div class="AppNavigation">
+        <div class="AppNavigation__header">
+          Header
+        </div>
+        <div class="ComponentScroll AppNavigation__main">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum debitis est laborum. Laborum ducimus totam fuga esse sunt praesentium odit ullam saepe exercitationem, soluta assumenda asperiores at vero aliquam sint!</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum debitis est laborum. Laborum ducimus totam fuga esse sunt praesentium odit ullam saepe exercitationem, soluta assumenda asperiores at vero aliquam sint!</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum debitis est laborum. Laborum ducimus totam fuga esse sunt praesentium odit ullam saepe exercitationem, soluta assumenda asperiores at vero aliquam sint!</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum debitis est laborum. Laborum ducimus totam fuga esse sunt praesentium odit ullam saepe exercitationem, soluta assumenda asperiores at vero aliquam sint!</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum debitis est laborum. Laborum ducimus totam fuga esse sunt praesentium odit ullam saepe exercitationem, soluta assumenda asperiores at vero aliquam sint!</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum debitis est laborum. Laborum ducimus totam fuga esse sunt praesentium odit ullam saepe exercitationem, soluta assumenda asperiores at vero aliquam sint!</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum debitis est laborum. Laborum ducimus totam fuga esse sunt praesentium odit ullam saepe exercitationem, soluta assumenda asperiores at vero aliquam sint!</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum debitis est laborum. Laborum ducimus totam fuga esse sunt praesentium odit ullam saepe exercitationem, soluta assumenda asperiores at vero aliquam sint!</p>
+        </div>
+        <div class="AppNavigation__footer">
+          Footer
+        </div>
+      </div>
+      <main class="AppMain">
+        <router-view></router-view>
+      </main>
+    </div>
   </div>
 </template>
 
@@ -15,36 +59,174 @@ export default {
 <style lang="scss">
 body {
   margin: 0;
-}
-
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
 }
 
-main {
-  text-align: center;
-  margin-top: 40px;
+$sidebarWidth: 200px;
+$headerHeight: 40px;
+
+// Element inside App Header and Navbar
+%NavBar__cell {
+  padding: 8px 10px;
 }
 
-header {
-  margin: 0;
-  height: 56px;
-  padding: 0 16px 0 24px;
-  background-color: #35495E;
-  color: #ffffff;
+.App {
+  &__version {
+    color: #91b8e0;
+  }
 }
 
-header span {
-  display: block;
-  position: relative;
-  font-size: 20px;
-  line-height: 1;
-  letter-spacing: .02em;
-  font-weight: 400;
+.AppHeader {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+
+  color: #fff;
+  background-color: #3374b9;
+  padding: 3px 0;
+
+  & {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+  }
+  &__brand {
+    @extend %NavBar__cell;
+  }
+  &__navigation {}
+
+  // Override NavBar Color
+  .NavBar__panel {
+  }
+}
+
+// Header Text Style
+.AppHeader {
+  &__brand,
+  .NavBar__cell {
+    line-height: 18px;
+    font-size: 14px;
+    font-weight: bold;
+  }
+}
+
+.AppContainer {
+  padding-left: $sidebarWidth;
+}
+
+.AppNavigation {
+  position: fixed;
+  top: $headerHeight;
+  left: 0;
+  bottom: 0;
+  width: $sidebarWidth;
+
+  display: flex;
+  flex-direction: column;
+
   box-sizing: border-box;
-  padding-top: 16px;
+  border-right: 1px solid;
+
+  &__header,
+  &__footer,
+  &__main {
+  }
+
+  &__header,
+  &__footer {
+    padding: 5px 8px;
+    background-color: #dfe8f1;
+  }
+
+  &__main {
+    flex-grow: 1;
+  }
+}
+
+.AppMain {
+  margin-top: $headerHeight;
+  overflow: hidden; // Force margin collapse
+
+  $spacingY: 40px;
+  &__view {
+    margin-left: $spacingY;
+    margin-right: $spacingY;
+  }
+}
+
+.NavBar {
+  $s: &; // Store scope selector
+  display: flex;
+  flex-direction: row;
+
+  & {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+  }
+  &__item {
+    position: relative;
+  }
+
+  &__cell {
+    @extend %NavBar__cell;
+
+    // Links only
+    @at-root a#{&} {
+      display: inline-block;
+      color: currentColor;
+      text-decoration: none;
+    }
+  }
+
+  &__cell {
+    // Reads: When Cell is child of Hover Item and an A element
+    // Reads: .NavBar__item:hover a.NavBar__cell
+    @at-root #{$s}__item:hover a#{&} {
+      text-decoration: underline;
+    }
+  }
+
+  &__panel {
+    position: absolute;
+    top: 100%;
+    left: 0;
+    display: none;
+
+    // Is list overrides
+    @at-root ul#{&} {
+      margin: 0;
+      padding: 0;
+      list-style: none;
+    }
+
+    color: #333;
+    background-color: #fff;
+    border: 1px solid rgba(0,0,0,0.1);
+    box-shadow: 0 1px 3px rgba(0,0,0,0.1);
+
+    // Positioning Details
+    @at-root ul#{&},
+    & {
+      margin-top: -2px;
+    }
+  }
+
+  // Open Panel
+  &__item:hover > &__panel {
+    display: block;
+  }
+}
+
+// Component with content scroll
+.ComponentScroll {
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  -webkit-overflow-scrolling: touch;
 }
 </style>
---
+++ src/components/Dashboard.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="Dashboard AppMain__view">
+    <h1>Dashboard</h1>
+    <p>{{ msg }}</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum debitis est laborum. Laborum ducimus totam fuga esse sunt praesentium odit ullam saepe exercitationem, soluta assumenda asperiores at vero aliquam sint!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum debitis est laborum. Laborum ducimus totam fuga esse sunt praesentium odit ullam saepe exercitationem, soluta assumenda asperiores at vero aliquam sint!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum debitis est laborum. Laborum ducimus totam fuga esse sunt praesentium odit ullam saepe exercitationem, soluta assumenda asperiores at vero aliquam sint!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum debitis est laborum. Laborum ducimus totam fuga esse sunt praesentium odit ullam saepe exercitationem, soluta assumenda asperiores at vero aliquam sint!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum debitis est laborum. Laborum ducimus totam fuga esse sunt praesentium odit ullam saepe exercitationem, soluta assumenda asperiores at vero aliquam sint!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum debitis est laborum. Laborum ducimus totam fuga esse sunt praesentium odit ullam saepe exercitationem, soluta assumenda asperiores at vero aliquam sint!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum debitis est laborum. Laborum ducimus totam fuga esse sunt praesentium odit ullam saepe exercitationem, soluta assumenda asperiores at vero aliquam sint!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum debitis est laborum. Laborum ducimus totam fuga esse sunt praesentium odit ullam saepe exercitationem, soluta assumenda asperiores at vero aliquam sint!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum debitis est laborum. Laborum ducimus totam fuga esse sunt praesentium odit ullam saepe exercitationem, soluta assumenda asperiores at vero aliquam sint!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum debitis est laborum. Laborum ducimus totam fuga esse sunt praesentium odit ullam saepe exercitationem, soluta assumenda asperiores at vero aliquam sint!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum debitis est laborum. Laborum ducimus totam fuga esse sunt praesentium odit ullam saepe exercitationem, soluta assumenda asperiores at vero aliquam sint!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum debitis est laborum. Laborum ducimus totam fuga esse sunt praesentium odit ullam saepe exercitationem, soluta assumenda asperiores at vero aliquam sint!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum debitis est laborum. Laborum ducimus totam fuga esse sunt praesentium odit ullam saepe exercitationem, soluta assumenda asperiores at vero aliquam sint!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum debitis est laborum. Laborum ducimus totam fuga esse sunt praesentium odit ullam saepe exercitationem, soluta assumenda asperiores at vero aliquam sint!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum debitis est laborum. Laborum ducimus totam fuga esse sunt praesentium odit ullam saepe exercitationem, soluta assumenda asperiores at vero aliquam sint!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum debitis est laborum. Laborum ducimus totam fuga esse sunt praesentium odit ullam saepe exercitationem, soluta assumenda asperiores at vero aliquam sint!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum debitis est laborum. Laborum ducimus totam fuga esse sunt praesentium odit ullam saepe exercitationem, soluta assumenda asperiores at vero aliquam sint!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum debitis est laborum. Laborum ducimus totam fuga esse sunt praesentium odit ullam saepe exercitationem, soluta assumenda asperiores at vero aliquam sint!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum debitis est laborum. Laborum ducimus totam fuga esse sunt praesentium odit ullam saepe exercitationem, soluta assumenda asperiores at vero aliquam sint!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum debitis est laborum. Laborum ducimus totam fuga esse sunt praesentium odit ullam saepe exercitationem, soluta assumenda asperiores at vero aliquam sint!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum debitis est laborum. Laborum ducimus totam fuga esse sunt praesentium odit ullam saepe exercitationem, soluta assumenda asperiores at vero aliquam sint!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum debitis est laborum. Laborum ducimus totam fuga esse sunt praesentium odit ullam saepe exercitationem, soluta assumenda asperiores at vero aliquam sint!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum debitis est laborum. Laborum ducimus totam fuga esse sunt praesentium odit ullam saepe exercitationem, soluta assumenda asperiores at vero aliquam sint!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum debitis est laborum. Laborum ducimus totam fuga esse sunt praesentium odit ullam saepe exercitationem, soluta assumenda asperiores at vero aliquam sint!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum debitis est laborum. Laborum ducimus totam fuga esse sunt praesentium odit ullam saepe exercitationem, soluta assumenda asperiores at vero aliquam sint!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum debitis est laborum. Laborum ducimus totam fuga esse sunt praesentium odit ullam saepe exercitationem, soluta assumenda asperiores at vero aliquam sint!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Dashboard',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js PWA',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+</style>
